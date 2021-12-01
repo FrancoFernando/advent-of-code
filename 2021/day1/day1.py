8ea1c9aa1c8d3409f506a80d35f82c5dd05bb61b
Franco Fernando <me@francofernando.com>
2021-12-01 15:45:49 +0100

    add day 1

---
+++ 2021/day1/day1.py
@@ -0,0 +1,34 @@
+# input preprocessing
+def process_input():
+    with open("Advent/day1/input.txt") as file:
+        input = file.readlines()
+    return [int(i) for i in input]
+
+# challenge 1
+def count_measurement_increments(input):
+
+    counter = 0
+
+    for i in range(1, len(input)):
+        if input[i] > input[i - 1]:
+            counter += 1
+
+    return counter
+
+# challenge 2
+def count_window_increments(input):
+
+    window_size = 3
+
+    counter = 0
+    rolling_sum = sum(input[:window_size])
+
+    for i in range(3, len(input)):
+        prev_sum = rolling_sum
+        rolling_sum -= input[i-window_size]
+        rolling_sum += input[i]
+        if rolling_sum > prev_sum:
+            counter += 1
+
+    return counter
+
