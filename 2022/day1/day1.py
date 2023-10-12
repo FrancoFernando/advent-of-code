e794e2c90e84e80ae5897f735123f0a377f865e3
fernando franco <fernando.franco@varian.com>
2023-10-12 23:05:36 +0200

    encapsulate in functions

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -1,17 +1,28 @@
-with open("2022/day1/input.txt", 'r') as file:
+def parse_input():
+    with open("2022/day1/input.txt", 'r') as file:
 
-    calories = [0]
-    
-    for line in file:
+        calories = [0]
         
-        if line.strip():
-            calories[-1] += int(line.strip())
-        else:
-            calories.append(0)
+        for line in file:
+            
+            if line.strip():
+                calories[-1] += int(line.strip())
+            else:
+                calories.append(0)
+
+    return calories
+
+def part_one(calories):
 
     result = max(calories)
     print(result)
 
+def part_two(calories):
     result = sum(sorted(calories, reverse=True)[:3])
     print(result)
+
+if __name__ == '__main__':
+    calories = parse_input()
+    part_one(calories)
+    part_two(calories)
             
