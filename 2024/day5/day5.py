b7b10d7e56b86b5d402be375e08b2fbf18ad25d8
francofernando <me@francofernando.com>
2024-12-05 09:50:05 +0100

    add day 5

---
+++ 2024/day5/day5.py
@@ -0,0 +1,101 @@
+from collections import defaultdict
+
+class Solution:
+
+    def __init__(self):
+        self.rules = defaultdict(set)
+        self.updates = []
+        self.parse_input()
+
+    def parse_input(self):
+        
+        with open("input.txt", 'r') as file:
+            # Parse rules
+            for line in file:
+                if line.strip() == "":
+                    break
+                a, b = map(int, line.strip().split('|'))
+                self.rules[a].add(b)
+            
+            # Parse updates
+            for line in file:
+                self.updates.append(list(map(int, line.strip().split(','))))
+
+    def topological_sort(self, graph, nodes):
+        in_degree = {node: 0 for node in nodes}
+        for node in nodes:
+            for neighbor in graph[node]:
+                if neighbor in nodes:
+                    in_degree[neighbor] += 1
+        
+        queue = [node for node in nodes if in_degree[node] == 0]
+        result = []
+        
+        while queue:
+            node = queue.pop(0)
+            result.append(node)
+            for neighbor in graph[node]:
+                if neighbor in nodes:
+                    in_degree[neighbor] -= 1
+                    if in_degree[neighbor] == 0:
+                        queue.append(neighbor)
+        
+        return result if len(result) == len(nodes) else None
+
+
+    def part_one(self): 
+        valid_updates = []
+        result = 0
+    
+        for update in self.updates:
+            subgraph = {page: self.rules[page].intersection(set(update)) for page in update}
+            sorted_pages = self.topological_sort(subgraph, update)
+            
+            if sorted_pages == update:
+                mid_index = len(update) // 2
+                result += update[mid_index]
+                valid_updates.append(update)
+
+        print(result)
+        return valid_updates
+    
+    def order_update(self, update):
+        subgraph = {page: self.rules[page].intersection(set(update)) for page in update}
+        sorted_pages = self.topological_sort(subgraph, update)
+        
+        if sorted_pages:
+            return sorted_pages
+        else:
+            # If topological sort fails, use a greedy approach
+            remaining = set(update)
+            ordered = []
+            while remaining:
+                # Find a page that has no predecessors in the remaining set
+                for page in remaining:
+                    if not any(pred in remaining for pred in self.rules if page in self.rules[pred]):
+                        ordered.append(page)
+                        remaining.remove(page)
+                        break
+                else:
+                    # If no such page is found, we have a cycle. Break it arbitrarily.
+                    page = remaining.pop()
+                    ordered.append(page)
+            return ordered
+    
+    def part_two(self):
+        reordered = []
+        result = 0
+        for update in self.updates:
+            reordered_update = self.order_update(update)
+            reordered.append(reordered_update)
+            mid_index = len(reordered_update) // 2
+            result += update[mid_index]
+        print(result)
+        return reordered
+        
+
+
+if __name__ == '__main__':
+    day1 = Solution() 
+    day1.part_one()
+    day1.part_two()
